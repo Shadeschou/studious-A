cd6c5f662285c4dd6b00b2ceb7b38447cdf9385e
Shadeschou <capper2650@hotmail.com>
2019-09-25 14:10:11 +0200

    changes

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -4,12 +4,13 @@ public class ProjectA {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int hours;
-        double hourlyPayment;
-        String employeName;
+        double hourlyPayment = 90;
+        String employeName = "Kasper";
         final int OVERTIME = 40;
         final double OVERTIMEPAYMENT = 1.5;
-        
-        employeName = "kasper";
 
+        if (in.hasNext()){
+            hours = in.nextInt();
+        }
     }
 }
