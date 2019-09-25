3c268f95d33034f2ff53e12634697e40d574e124
Shadeschou <capper2650@hotmail.com>
2019-09-25 13:36:15 +0200

    Declaring variables

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -1,5 +1,11 @@
 public class ProjectA {
     public static void main(String[] args) {
-        int Temp = 0;
+        int hours;
+        double hourlyPayment;
+        String employeName;
+        final int OVERTIME = 40;
+        final double OVERTIMEPAYMENT = 1.5;
+        
+
     }
 }
