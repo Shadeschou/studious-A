5689d4e79ff98ee1ce2832ae742ef788e20323d0
Shadeschou <capper2650@hotmail.com>
2019-09-25 13:52:22 +0200

    lol

--- src/ProjectA.java
+++ src/ProjectA.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class ProjectA {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int hours;
         double hourlyPayment;
         String employeName;
         final int OVERTIME = 40;
         final double OVERTIMEPAYMENT = 1.5;
+        
         
 
     }
